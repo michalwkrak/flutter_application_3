95a685a7cf9f6bf306d01c2dd9241def15c70925
michalwkrak <michal.wojciga@gmail.com>
2024-07-01 20:00:40 +0200

    Zmiana adresu swerwera FLASK na ten hostowany na RasberryPI

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,4 @@
 class Config {
-  static const String serverIp = 'http://127.0.0.1:5000';
+  //static const String serverIp = 'http://127.0.0.1:5000';
+  static const String serverIp = 'http://45.66.23.54:10158';
 }
